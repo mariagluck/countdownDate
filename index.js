7916842067c1d99ad6c21430752a84ffc0a3904b
mariagluck <mariamart.berlin@gmail.com>
2021-08-19 14:35:47 +0200

    updated countdown date

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const anniversaryDate = new Date("Jul 24, 2021 00:00:00").getTime();
+const anniversaryDate = new Date("Jul 24, 2031 00:00:00").getTime();
 
 // Run myfunc every second
 const countDownToDate = setInterval(function() {
@@ -11,6 +11,7 @@ let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
 let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
     
 // Result is output to the specific element
 document.getElementById("days").innerHTML = `
